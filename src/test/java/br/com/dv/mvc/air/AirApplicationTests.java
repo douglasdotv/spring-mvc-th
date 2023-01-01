1faa59a10613396143d9187c48baa3aff4954ec3
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-01 09:24:31 -0300

    Start project with Spring Initializr

---
+++ src/test/java/br/com/dv/mvc/air/AirApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dv.mvc.air;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
