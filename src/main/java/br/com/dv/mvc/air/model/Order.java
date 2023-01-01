3a2b205937d8cf0dd8d027c5a642113b04302adf
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-01 17:00:23 -0300

    Create 'Order' model

---
+++ src/main/java/br/com/dv/mvc/air/model/Order.java
@@ -0,0 +1,63 @@
+package br.com.dv.mvc.air.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Order {
+
+    private String productName;
+    private BigDecimal price;
+    private LocalDate deliveryDate;
+    private String productDescription;
+    private String productUrl;
+    private String imageUrl;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
