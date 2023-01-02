62e9efdfee46808d16da6494d6690163dfd3a344
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-01 22:29:33 -0300

    Implement db access in home page controller

--- src/main/java/br/com/dv/mvc/air/controller/HomeController.java
+++ src/main/java/br/com/dv/mvc/air/controller/HomeController.java
@@ -1,6 +1,9 @@
 package br.com.dv.mvc.air.controller;
 
 import br.com.dv.mvc.air.model.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +13,14 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model) {
-        Order order = new Order();
-
-        order.setProductName("Smartphone Xiaomi Redmi 9A 32GB/2GB Ram Azul");
-        order.setProductUrl("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-9A-32GB/dp/B08CH6X6M5/ref=sr_1_1?qid=1672603320&refinements=p_89%3AXiaomi&s=electronics&sr=1-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-        order.setImageUrl("https://m.media-amazon.com/images/I/51bseWUDNOL._AC_SX679_.jpg");
-        order.setProductDescription("Smartphone - Redmi 9A - 2GB - 32GB - Sky Blue - Xiaomi");
-
-        List<Order> orderList = List.of(order);
+        Query query = entityManager.createQuery("Select p from Order p", Order.class);
+        List<Order> orderList = query.getResultList();
         model.addAttribute("orderList", orderList);
-
         return "home";
     }
 
