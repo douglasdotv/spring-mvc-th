e89b74b3ec2693ebd0bb4b7d1e576ad3137fb3e3
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-02 05:46:54 -0300

    Create Order Data Transfer Object class

---
+++ src/main/java/br/com/dv/mvc/air/model/OrderDTO.java
@@ -0,0 +1,42 @@
+package br.com.dv.mvc.air.model;
+
+public class OrderDTO {
+
+    private String productName;
+    private String description;
+    private String productUrl;
+    private String imageUrl;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
