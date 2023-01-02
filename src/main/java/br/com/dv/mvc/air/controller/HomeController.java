523f2eef58b29b38bbb912a3fc51f7d39a4a0473
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-02 00:39:18 -0300

    Remove unused annotation

--- src/main/java/br/com/dv/mvc/air/controller/HomeController.java
+++ src/main/java/br/com/dv/mvc/air/controller/HomeController.java
@@ -4,7 +4,6 @@ import br.com.dv.mvc.air.model.Order;
 import br.com.dv.mvc.air.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
